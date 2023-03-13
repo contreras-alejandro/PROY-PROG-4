777f314a61ddc8b706e19710b80bf68221182d7e
ikerruesgas <i.ruesgas@opendeusto.es>
2023-03-13 15:37:33 +0100

    Primer Menu

--- Admin/Main/main.c
+++ Admin/Main/main.c
@@ -1,1 +1,32 @@
 #include <stdio.h>
+#include <windows.h>
+
+int main() {
+    HANDLE hConsole = GetStdHandle( STD_OUTPUT_HANDLE );
+    SetConsoleTextAttribute(hConsole, 1);
+    int opcion;
+    
+    do {
+        printf("________\n");
+        printf("||MENU||\n");
+        printf("________\n");
+        printf("**Bienvenido al programe de administración del servidor**\n");
+        printf("1. Inicar sesión\n");
+        printf("2. Salir\n");
+        printf("Seleccione una opción: ");
+        scanf("%d", &opcion);
+        
+        switch (opcion) {
+            case 1:
+                printf("Has seleccionado la opción 1: INICIAR SESIÓN.\n");
+                break;
+            case 2:
+                printf("Saliendo del programa\n");
+                break;
+            default:
+                printf("¡ERROR, SELECCIONE UN NUMERO!\n");
+        }
+    } while (opcion != 2);
+    
+    return 0;
+}
